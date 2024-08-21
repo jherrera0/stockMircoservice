923daf0795bf50c41ba76ef10faf46eda8b7341b
Jose Herrera <jherreraa2@unicartagena.edu.co>
2024-08-20 19:52:42 -0500

    initial commit

---
+++ src/main/java/bootcamp/stockMircoservice/StockMircoserviceApplication.java
@@ -0,0 +1,13 @@
+package bootcamp.stockMircoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMircoserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMircoserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bootcamp/stockMircoservice/StockMircoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package bootcamp.stockMircoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMircoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
