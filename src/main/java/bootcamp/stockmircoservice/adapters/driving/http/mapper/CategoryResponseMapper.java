bc289898876fb4d74645342c4553cf4a727a1981
Jose Herrera <jherreraa2@unicartagena.edu.co>
2024-08-21 22:42:40 -0500

    chages to implement good pratics

--- src/main/java/bootcamp/stockmircoservice/adapters/driving/http/mapper/CategoryResponseMapper.java
+++ src/main/java/bootcamp/stockmircoservice/adapters/driving/http/mapper/CategoryResponseMapper.java
@@ -27,4 +27,5 @@ public interface CategoryResponseMapper {
                     return categoryResponse;
                 }).toList();
     }
+
 }
--- src/test/java/bootcamp/stockmircoservice/StockMircoserviceApplicationTests.java
+++ src/test/java/bootcamp/stockmircoservice/StockMircoserviceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class StockMircoserviceApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() {// default implementation ignored
 	}
 
 }
