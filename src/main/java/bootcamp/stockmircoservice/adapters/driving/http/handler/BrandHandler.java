4104d8ce7de17d8a645ea42840c79ab27d6e429e
Jose Herrera <jherreraa2@unicartagena.edu.co>
2024-08-27 12:32:53 -0500

    update domain and adapter http to implement UH 4

--- src/main/java/bootcamp/stockmircoservice/adapters/driving/http/handler/BrandHandler.java
+++ src/main/java/bootcamp/stockmircoservice/adapters/driving/http/handler/BrandHandler.java
@@ -1,8 +1,10 @@
 package bootcamp.stockmircoservice.adapters.driving.http.handler;
 
 import bootcamp.stockmircoservice.adapters.driving.http.dto.request.BrandRequest;
+import bootcamp.stockmircoservice.adapters.driving.http.dto.response.BrandResponse;
 import bootcamp.stockmircoservice.adapters.driving.http.handler.interfaces.IBrandHandler;
 import bootcamp.stockmircoservice.adapters.driving.http.mapper.BrandRequestMapper;
+import bootcamp.stockmircoservice.adapters.driving.http.mapper.BrandResponseMapper;
 import bootcamp.stockmircoservice.domain.api.IBrandServicePort;
 
 import bootcamp.stockmircoservice.domain.model.Brand;
@@ -10,17 +12,25 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class BrandHandler implements IBrandHandler {
 
     private final BrandRequestMapper brandRequestMapper;
+    private final BrandResponseMapper brandResponseMapper;
     private final IBrandServicePort brandServicePort;
 
     @Override
     public void saveBrand(BrandRequest brandRequest) {
         Brand brand = brandRequestMapper.toBrand(brandRequest);
         brandServicePort.saveBrand(brand);
+    }
+
+    @Override
+    public List<BrandResponse> getAllBrands(Integer page, Integer size, String sortDirection) {
+        return brandResponseMapper.toResponseList(brandServicePort.getAllBRands(page, size, sortDirection));
     }
 }
--- src/main/java/bootcamp/stockmircoservice/domain/api/IBrandServicePort.java
+++ src/main/java/bootcamp/stockmircoservice/domain/api/IBrandServicePort.java
@@ -1,7 +1,11 @@
 package bootcamp.stockmircoservice.domain.api;
 
 import bootcamp.stockmircoservice.domain.model.Brand;
+import bootcamp.stockmircoservice.domain.model.Category;
+
+import java.util.List;
 
 public interface IBrandServicePort {
     void saveBrand(Brand brand);
+    List<Brand> getAllBRands(Integer page, Integer size, String sortDirection);
 }
