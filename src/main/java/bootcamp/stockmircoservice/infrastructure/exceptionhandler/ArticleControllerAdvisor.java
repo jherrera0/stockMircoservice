e8a13f38bc42b416322c0e19975dcb6911637e20
Jose Herrera <jherreraa2@unicartagena.edu.co>
2024-08-29 23:41:42 -0500

    delete unused import

--- src/main/java/bootcamp/stockmircoservice/infrastructure/exceptionhandler/ArticleControllerAdvisor.java
+++ src/main/java/bootcamp/stockmircoservice/infrastructure/exceptionhandler/ArticleControllerAdvisor.java
@@ -1,7 +1,6 @@
 package bootcamp.stockmircoservice.infrastructure.exceptionhandler;
 
 import bootcamp.stockmircoservice.infrastructure.exception.article.DuplicateCategoriesException;
-import bootcamp.stockmircoservice.infrastructure.exception.brand.BrandNullFieldException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
--- src/main/java/bootcamp/stockmircoservice/infrastructure/input/rest/ArticleRestController.java
+++ src/main/java/bootcamp/stockmircoservice/infrastructure/input/rest/ArticleRestController.java
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
