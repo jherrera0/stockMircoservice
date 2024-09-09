cdd001be45045d079cab908a0a4afdd7a90cbc7c
Jose Herrera <jherreraa2@unicartagena.edu.co>
2024-09-08 21:43:59 -0500

    brand

--- src/main/java/bootcamp/stockmircoservice/domain/model/Brand.java
+++ src/main/java/bootcamp/stockmircoservice/domain/model/Brand.java
@@ -12,12 +12,6 @@ public class Brand {
         this.description = description;
     }
 
-    public Brand(Long id, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
-
     public Long getId() {
         return id;
     }
